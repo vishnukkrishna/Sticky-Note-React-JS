469723b8201c4ac2942f15389f9e25dc8798c342
Vishnu Krishnakumar <83391274+vishnukkrishna@users.noreply.github.com>
2024-02-27 00:48:50 +0530

    first commit

---
+++ src/components/Note/Note.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Note.css";
+
+function Note(props) {
+  {
+    test: "qwerty";
+    time: "10:00 AM";
+    color: "red";
+  }
+  return (
+    <div className="note">
+      <textarea className="note_text" name="" id="" />
+      <p>10.00 PM 22 May</p>
+    </div>
+  );
+}
+
+export default Note;
