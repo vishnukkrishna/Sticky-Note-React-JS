a856b39317eea6a3982c1d12523865d2d57b035e
Vishnu Krishnakumar <83391274+vishnukkrishna@users.noreply.github.com>
2024-02-27 17:46:28 +0530

    final Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import NoteContainer from "./components/NoteContainer/NoteContainer";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
+  const [notes, setNotes] = useState(
+    JSON.parse(localStorage.getItem("notes-app")) || []
+  );
+
+  const addNote = (color) => {
+    const tempNotes = [...notes];
+
+    tempNotes.push({
+      id: Date.now() + "" + Math.floor(Math.random() * 78),
+      text: "",
+      time: Date.now(),
+      color,
+    });
+    setNotes(tempNotes);
+  };
+
+  const deleteNote = (id) => {
+    const tempNotes = [...notes];
+
+    const index = tempNotes.findIndex((item) => item.id === id);
+    if (index < 0) return;
+
+    tempNotes.splice(index, 1);
+    setNotes(tempNotes);
+  };
+
+  const updateText = (text, id) => {
+    const tempNotes = [...notes];
+
+    const index = tempNotes.findIndex((item) => item.id === id);
+    if (index < 0) return;
+
+    tempNotes[index].text = text;
+    setNotes(tempNotes);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("notes-app", JSON.stringify(notes));
+  }, [notes]);
+
   return (
     <div className="App">
-      <NoteContainer />
+      <Sidebar addNote={addNote} />
+      <NoteContainer
+        notes={notes}
+        deleteNote={deleteNote}
+        updateText={updateText}
+      />
     </div>
   );
 }
--- src/components/Note/Note.jsx
+++ src/components/Note/Note.jsx
@@ -1,16 +1,67 @@
 import React from "react";
 import "./Note.css";
+import deleteIcon from "../../assets/delete.png";
 
 function Note(props) {
-  {
-    test: "qwerty";
-    time: "10:00 AM";
-    color: "red";
-  }
+  const formatDate = (value) => {
+    if (!value) return "";
+
+    const date = new Date(value);
+    const monthNames = [
+      "Jan",
+      "Feb",
+      "March",
+      "April",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sept",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+
+    let hrs = date.getHours();
+    let amPm = hrs >= 12 ? "PM" : "AM";
+    hrs = hrs ? hrs : "12";
+    hrs = hrs > 12 ? (hrs = 24 - hrs) : hrs;
+
+    let min = date.getMinutes();
+    min = min < 10 ? "0" + min : min;
+
+    let day = date.getDate();
+    const month = monthNames[date.getMonth()];
+
+    return `${hrs}:${min} ${amPm} ${day} ${month}`;
+  };
+
+  let timeout;
+  const timer = 500;
+
+  const debounce = (func) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(func, timer);
+  };
+
+  const updateText = (text, id) => {
+    debounce(() => props.updateText(text, id));
+  };
   return (
-    <div className="note">
-      <textarea className="note_text" name="" id="" />
-      <p>10.00 PM 22 May</p>
+    <div className="note" style={{ backgroundColor: props.note.color }}>
+      <textarea
+        className="note_text"
+        defaultValue={props.note.text}
+        onChange={(event) => updateText(event.target.value, props.note.id)}
+      />
+      <div className="note_footer">
+        <p>{formatDate(props.note.time)}</p>
+        <img
+          src={deleteIcon}
+          alt="DELETE"
+          onClick={() => props.deleteNote(props.note.id)}
+        />
+      </div>
     </div>
   );
 }
